e158d6bc6d6b2c88823e4dd46bfcdfcfb636ea6c
nawojmonika <nawojmonika@gmail.com>
2019-03-19 10:05:10 +0100

    changed names

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.crypto.Cipher;
+
 public class Main extends Application {
+    private static Cipher encryptCipher;
+    private static Cipher descryptCipher;
+    private static  final byte[] iv = {11,22,33,44,99,88,77,66};
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("DES encryptor");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
