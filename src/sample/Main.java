bdc27130bbc0e038d5bf0cfccbbd30776f3d0a95
nawojmonika <nawojmonika@gmail.com>
2019-03-23 17:08:09 +0100

    Added constraint permutation table

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import javax.crypto.Cipher;
-
 public class Main extends Application {
-    private static Cipher encryptCipher;
-    private static Cipher descryptCipher;
-    private static  final byte[] iv = {11,22,33,44,99,88,77,66};
+
+    private final byte[] IP = {58, 50, 42, 34, 26, 18, 10, 2,
+                         60, 52, 44, 36, 28, 20, 12, 4,
+                         62, 54, 46, 38, 30, 22, 14, 6,
+                         64, 56, 48, 40, 32, 24, 16, 8,
+                         57, 49, 41, 33, 25, 17, 9, 1,
+                         59, 51, 43, 35, 27, 19, 11, 3,
+                         61, 53, 45, 37, 29, 21, 13, 5,
+                         63, 55, 47, 39, 31, 23, 15, 7};
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -21,8 +26,11 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void initialPermutaion(){
+
     }
 }
