551e128e5de317b8eeccd1de563eebf62cffd01a
nawojmonika <nawojmonika@gmail.com>
2019-03-24 18:31:03 +0100

    Added actual IP value

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Main extends Application {
 
-    private static int[] IP = {57, 49, 41, 33, 25, 17, 9, 1,
+    private static int[] initialKeyPermutation = {57, 49, 41, 33, 25, 17, 9, 1,
             58, 50, 42, 34, 26, 18, 10, 2,
             59, 51, 43, 35, 27, 19, 11, 3,
             60, 52, 44, 36, 63, 55, 47, 39,
@@ -18,14 +18,23 @@ public class Main extends Application {
             30, 22, 14, 6, 61, 53, 45, 37,
             29, 21, 13, 5, 28, 20, 12, 4};
 
-    private static int[] rotationTable= {1,	1,	2,	2,	2,	2,	2,	2,	1,	2,	2,	2,	2,	2,	2, 1 };
+    private static int[] rotationTable= {1,	1, 2, 2, 2, 2, 2, 2, 1,	2, 2, 2, 2, 2, 2, 1 };
 
-    private static int[] keyPermutation = {14,	17,	11,	24,	1,	5,	3,	28,
+    private static int[] subkeyPermutation = {14,	17,	11,	24,	1,	5,	3,	28,
             15,	6,	21,	10,	23,	19,	12,	4,
             26,	8,	16,	7, 27,	20,	13,	2,
             41,	52,	31,	37,	47,	55,	30,	40,
             51,	45,	33,	48,	44,	49,	39,	56,
             34,	53,	46,	42,	50,	36,	29,	32};
+
+    public static int[] IP = {58, 50, 42, 34, 26, 18, 10, 2,
+            60, 52, 44, 36, 28, 20, 12, 4,
+            62, 54, 46, 38, 30, 22, 14, 6,
+            64, 56, 48, 40, 32, 24, 16, 8,
+            57, 49, 41, 33, 25, 17, 9, 1,
+            59, 51, 43, 35, 27, 19, 11, 3,
+            61, 53, 45, 37, 29, 21, 13, 5,
+            63, 55, 47, 39, 31, 23, 15, 7};
 
     private String originalMessage = "IEOFIT#2";
     private String key = "IEOFIT#1";
@@ -47,7 +56,7 @@ public class Main extends Application {
 
     private ArrayList<String> getEncryptedKeys(String key) {
         String biteKey = this.getBites(key);
-        String permutatedKey = this.permutation(biteKey, this.IP);
+        String permutatedKey = this.permutation(biteKey, this.initialKeyPermutation);
         ArrayList<String> subKeys = new ArrayList<String>();
         int keyLength = permutatedKey.length();
         String leftSide = permutatedKey.substring(0, keyLength / 2);
@@ -56,7 +65,7 @@ public class Main extends Application {
         for (int shift:rotationTable) {
             leftSide = this.leftShift(leftSide, shift);
             rightSide = this.leftShift(rightSide, shift);
-            String subKey = this.permutation(leftSide + rightSide, this.keyPermutation);
+            String subKey = this.permutation(leftSide + rightSide, this.subkeyPermutation);
             subKeys.add(subKey);
         }
         return subKeys;
