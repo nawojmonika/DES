f1a4cfe9308bda84475e592cd22e055724a5d273
grayrattus <arturdziedziczak0@gmail.com>
2019-03-25 19:06:12 +0100

    Add inputs setup

--- src/sample/ProgramWindow.java
+++ src/sample/ProgramWindow.java
@@ -2,16 +2,17 @@ package sample;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class ProgramWindow {
     public static Stage mainStage = null;
     public static Group root = null;
     public static Scene scene = null;
+    public static BorderPane borderPane = null;
+    public static GridPane inputsGrid = null;
 
     private static MenuBar mainMenu = null;
     private static Menu openFromFile = null;
@@ -19,18 +20,39 @@ public class ProgramWindow {
 
     private static MenuItem encrypt = null;
     private static MenuItem decrypt = null;
+    private static TextArea textToEncryption = null;
+    private static TextArea encryptedText = null;
 
 
     ProgramWindow(Stage mainStage) {
         this.mainStage = mainStage;
         mainStage.setTitle("DES program");
         root = new Group();
-        scene = new Scene(root, 400, 250);
+        scene = new Scene(root, 1200, 250);
+        borderPane = new BorderPane();
 
         setupMenu();
+        setupInputs();
 
         mainStage.setScene(scene);
         mainStage.show();
+    }
+
+    public void setupInputs() {
+        inputsGrid = new GridPane();
+
+        Label labelTextToEncryption = new Label("Text to encryption");
+        Label labelEncryptedText = new Label("Encrypted text");
+        textToEncryption = new TextArea();
+        encryptedText = new TextArea();
+
+        inputsGrid.add(labelTextToEncryption, 1, 1);
+        inputsGrid.add(textToEncryption, 1, 2);
+
+        inputsGrid.add(labelEncryptedText, 2, 1);
+        inputsGrid.add(encryptedText, 2, 2);
+
+        borderPane.setCenter(inputsGrid);
     }
 
     public void setupMenu() {
@@ -45,7 +67,6 @@ public class ProgramWindow {
 
         mainMenu.getMenus().addAll(openFromFile, cryptography);
 
-        BorderPane borderPane = new BorderPane();
         borderPane.setTop(mainMenu);
         root.getChildren().add(borderPane);
     }
